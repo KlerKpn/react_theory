5d12bb21a8344af717e73e0048a80a1cf3f33f48
KlerKpn <59790939+KlerKpn@users.noreply.github.com>
2020-08-01 09:35:24 +0300

    Radium, style.js , style.css
    Радиум позволяет юзать css в js файле
    подробнее в доках

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Radium from 'radium'
+import './car.css'
 
 // function car(){
 //     return (
@@ -12,19 +14,42 @@ import React from 'react'
 // </div>
 // )
 
-export default props => (
-<div style={{
-    border: '1px solid black',
-    marginBottom: '10px',
-    padding: '20px',
-    boxShadow:'0 4px 5px 0 rgba(0, 0, 0, .14)',
-    borderRadius: 5
-}}>
-    <p>{props.name}</p>
-    <strong>{props.year}</strong>
-    <br></br>
-    {/* <span>{Math.floor(Math.random() * 100)}</span> */}
-    <input type='text' onChange={props.onChangeName} value={props.name} />
-    <button onClick={props.onDelete}>Delete</button>
-    {props.children}
-</div>)
+const Car = props => {
+    const inputClasses = ['input']
+    
+    if (props.name !==""){
+        inputClasses.push('green')
+    } else {
+        inputClasses.push('red')
+    }
+    if (props.name.length >4){
+        inputClasses.push('bold')
+    } 
+
+    const style = {
+        border: '1px solid #ccc',
+        boxShadow:' 0 4px 5px 0 rgba(0, 0, 0, .14)',
+            ':hover':{
+                border: '1px solid #aaa',
+                boxShadow:' 0 4px 15px 0 rgba(0, 0, 0, .25)',
+            }
+    }
+
+    return (
+    <div className='Car' style={style}>
+        <p>Car name: {props.name}</p>
+        <strong>{props.year}</strong>
+        <br></br>
+        {/* <span>{Math.floor(Math.random() * 100)}</span> */}
+        <input 
+        type='text'
+        onChange={props.onChangeName} 
+        value={props.name}
+        className={inputClasses.join(' ')}
+         />
+        <button onClick={props.onDelete}>Delete</button>
+        {props.children}
+    </div>
+    )
+}
+export default Radium(Car)
